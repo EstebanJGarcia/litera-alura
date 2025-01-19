36f59e4ad87ba8b2cfefe26de6e80dc6a01fbb9e
Esteban  Jose Garcia Rios <168574528+EstebanJGarcia@users.noreply.github.com>
2025-01-19 15:12:54 -0300

    Add files via upload

---
+++ LiteraAlura/src/main/java/com/aluracursos/LiteraAlura/repository/AutorRepository.java
@@ -0,0 +1,25 @@
+package com.aluracursos.LiteraAlura.repository;
+
+import com.aluracursos.LiteraAlura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    @Query("SELECT a FROM Autor a WHERE a.birth_day <= :year AND (a.death_day IS NULL OR a.death_day >= :year) ORDER BY a.birth_day ASC")
+    List<Autor> findAuthorsAliveInYear(Integer year);
+
+    @Query("SELECT a FROM Autor a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<Autor> findByName(String name);
+
+    @Query("SELECT a FROM Autor a WHERE a.birth_day = :year")
+    List<Autor> findByBirthYear(Integer year);
+
+    @Query("SELECT a FROM Autor a WHERE a.death_day = :year")
+    List<Autor> findByDeathYear(Integer year);
+
+    boolean existsByName(String name);
+}
+
